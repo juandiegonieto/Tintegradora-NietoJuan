16dbeeefe0025f380fdfde306b885b929117dc76
juandiegonieto <61235591+juandiegonieto@users.noreply.github.com>
2020-09-09 08:02:22 -0500

    Create MaterialesADomicilio.java

---
+++ src/ui/MaterialesADomicilio.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MaterialesADomicilio {
+
+    public void intialice() {
+        final int ROUGH_CONSTRUCTION = 1300000;
+        final int WHITE_WORK = 2600000;
+        final int PAINTING = 980000;
+        String[] nombreDeProductos;
+        float[] productAmount;
+        int[] productUseDestination;
+        int answer, ubication, productsSize;     
+        boolean finished = false;
+    }
+
+    public 
+}
