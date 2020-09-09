de51b2264ca485d904d7bf4c93bc3e95733e7da4
juandiegonieto <61235591+juandiegonieto@users.noreply.github.com>
2020-09-09 08:07:42 -0500

    Update MaterialesADomicilio.java

--- src/ui/MaterialesADomicilio.java
+++ src/ui/MaterialesADomicilio.java
@@ -12,5 +12,26 @@ public class MaterialesADomicilio {
         boolean finished = false;
     }
 
-    public 
+    public int programMenu(boolean finished, int ubication, int answer) {
+
+        while(finished =! true ) {
+            System.out.println("----__________----");
+            System.out.println("----|  MENU  |----");
+            System.out.println("----**********----");
+            System.out.println("");
+            System.out.println("Ingrese su localidad: \n(1) Norte.\n(2) Centro\n(3) Sur");
+            ubication = in.nextInt();
+            System.out.println("");
+            System.out.println("Ingrese el numero: ");
+            System.out.println("(1.) Ingreso de Materiales.");
+            System.out.println("(2.) Salir y mostrar precio");
+            answer = in.nextInt();
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
